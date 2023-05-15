897b721a77e3f2b19435bf227d70a1b4df8ef3d5
njoro410 <brian.njoroge@student.moringaschool.com>
2023-05-15 11:47:01 +0300

    added a navbar and social links components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,25 @@
-import './globals.css'
-import { Open_Sans } from 'next/font/google'
+import "./globals.css";
+import { Open_Sans } from "next/font/google";
+import Navbar from "app/(shared)/Navbar";
 
-const OpenSans = Open_Sans({ subsets: ['latin'] })
+const OpenSans = Open_Sans({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Blog AI App',
-  description: 'Blog built in Next JS that uses AI',
-}
+  title: "Blog AI App",
+  description: "Blog built in Next JS that uses AI",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={OpenSans.className}>{children}</body>
+      <body className={OpenSans.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
-  )
+  );
 }
